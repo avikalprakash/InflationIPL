b9343de5bddaaf2dca6f16edfc5801d7f5fa4354
Kundan <kkscwcd@gmail.com>
2019-01-23 17:07:07 +0530

    Initial commit

---
+++ app/src/main/java/patna/bihar/inflaction/MainActivity.java
@@ -0,0 +1,13 @@
+package patna.bihar.inflaction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
